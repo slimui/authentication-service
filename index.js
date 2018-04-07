f1358a99344a23d9e209bbf0a41903f685e1a67e
Harrison Harnisch <hharnisc@gmail.com>
2018-04-08 07:30:40 +0800

    stub in working express app

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello, world'))
+
+app.listen(80, () => console.log('Started listening on port 80'))
