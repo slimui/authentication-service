1147b6d45b0f9a02bb73d40a256deb59d8830f8f
Harrison Harnisch <hharnisc@gmail.com>
2018-04-08 14:24:59 +0800

    implement password/reset/start endpoint

--- src/passwordResetStart.js
+++ src/passwordResetStart.js
@@ -1,1 +1,55 @@
-module.exports = ({ collectionClient }) => async (req, res) => res.send('ok')
+const { ObjectID } = require('mongodb')
+const uuid = require('uuid/v4')
+const Joi = require('joi')
+const {
+  validate,
+  parseValidationErrorMessage,
+} = require('./utils')
+
+const schema = Joi.object()
+  .keys({
+    email: Joi.string(),
+    id: Joi.string(),
+  })
+    .xor('email', 'id')
+
+module.exports = ({ collectionClient }) => async (req, res) => {
+  try {
+    await validate({
+      value: req.body,
+      schema
+    })
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: parseValidationErrorMessage({ error }),
+    })
+  }
+  const { email, id } = req.body
+  let query
+  if (email) {
+    query = { email }
+  } else {
+    query = {
+      _id: ObjectID(id),
+    }
+  }
+  const resetToken = uuid()
+  const result = await collectionClient.updateOne(query, {
+    $set: {
+      resetAt: new Date(),
+      resetToken,
+    },
+  })
+  if (result.matchedCount !== 1) {
+    res.status(400).send({
+      success: false,
+      message: `Could not start password reset on account with ${ email ? 'email' : 'id' }: ${ email ? email : id }`
+    })
+  } else {
+    res.send({
+      success: true,
+      resetToken,
+    })
+  }
+}
