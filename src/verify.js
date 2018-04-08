37a9247dbaa9361391b714f68be4e68e600aaa91
Harrison Harnisch <hharnisc@gmail.com>
2018-04-08 14:55:09 +0800

    add validation on verify endpoint

--- src/verify.js
+++ src/verify.js
@@ -1,1 +1,35 @@
-module.exports = ({ collectionClient }) => async (req, res) => res.send('OK')
+const Joi = require('joi')
+const {
+  validate,
+  parseValidationErrorMessage,
+} = require('./utils')
+
+const schema = Joi.object()
+  .keys({
+    email: Joi.string(),
+    id: Joi.string(),
+    password: Joi.string().required(),
+    productName: Joi.string(),
+    productToken: Joi.string(),
+    moderatorName: Joi.string(),
+    moderatorSecret: Joi.string(),
+  })
+    .xor('email', 'id')
+    .xor('productToken', 'moderatorName')
+    .with('moderatorName', 'moderatorSecret')
+
+
+module.exports = ({ collectionClient }) => async (req, res) => {
+  try {
+    await validate({
+      value: req.body,
+      schema
+    })
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: parseValidationErrorMessage({ error }),
+    })
+  }
+  res.send('OK')
+}
