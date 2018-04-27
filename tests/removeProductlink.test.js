a9f2437c3a6fea6f985c9849429b2aca4c55de1f
Harrison Harnisch <hharnisc@gmail.com>
2018-04-27 15:58:59 -0500

    add test: remove product link by email

--- tests/removeProductlink.test.js
+++ tests/removeProductlink.test.js
@@ -5,7 +5,7 @@ describe('removeProductlink', () => {
     expect(removeProductlink).toBeDefined()
   })
 
-  it('should remove a productlink', async () => {
+  it('should remove a productlink by _id', async () => {
     const _id = 'some user id'
     const productName = 'some product name'
     const AuthenticationAccountModel = {
@@ -24,6 +24,35 @@ describe('removeProductlink', () => {
     })
     expect(AuthenticationAccountModel.updateOne).toBeCalledWith(
       { $or: [{ _id }, { email: undefined }] },
+      {
+        $pull: {
+          productlinks: {
+            productName,
+          },
+        },
+      },
+    )
+  })
+
+  it('should remove a productlink by email', async () => {
+    const email = 'e@mail.com'
+    const productName = 'some product name'
+    const AuthenticationAccountModel = {
+      updateOne: jest.fn(() =>
+        Promise.resolve({
+          ok: 1,
+        }),
+      ),
+    }
+    const response = await removeProductlink({ AuthenticationAccountModel })({
+      email,
+      productName,
+    })
+    expect(response).toEqual({
+      success: true,
+    })
+    expect(AuthenticationAccountModel.updateOne).toBeCalledWith(
+      { $or: [{ _id: undefined }, { email }] },
       {
         $pull: {
           productlinks: {
