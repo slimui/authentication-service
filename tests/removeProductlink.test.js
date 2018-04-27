a50a14971a0a78ba40762822a3abde07dd56d30c
Harrison Harnisch <hharnisc@gmail.com>
2018-04-27 15:57:19 -0500

    add test: should remove a productlink

--- tests/removeProductlink.test.js
+++ tests/removeProductlink.test.js
@@ -4,4 +4,33 @@ describe('removeProductlink', () => {
   it('should export function', () => {
     expect(removeProductlink).toBeDefined()
   })
+
+  it('should remove a productlink', async () => {
+    const _id = 'some user id'
+    const productName = 'some product name'
+    const AuthenticationAccountModel = {
+      updateOne: jest.fn(() =>
+        Promise.resolve({
+          ok: 1,
+        }),
+      ),
+    }
+    const response = await removeProductlink({ AuthenticationAccountModel })({
+      _id,
+      productName,
+    })
+    expect(response).toEqual({
+      success: true,
+    })
+    expect(AuthenticationAccountModel.updateOne).toBeCalledWith(
+      { $or: [{ _id }, { email: undefined }] },
+      {
+        $pull: {
+          productlinks: {
+            productName,
+          },
+        },
+      },
+    )
+  })
 })
