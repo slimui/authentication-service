42bd55c17facd65b23937a31ee7d3ca8fc0c9eea
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 15:21:30 -0500

    test: should update a password by _id

--- tests/updatePassword.test.js
+++ tests/updatePassword.test.js
@@ -35,4 +35,35 @@ describe('updatePassword', () => {
       success: true,
     })
   })
+
+  it('should update a password by _id', async () => {
+    const _id = 'some id'
+    const password = 'password'
+    const newPassword = 'newPassword'
+    const someUser = {
+      verifyPassword: jest.fn(() => true),
+      save: jest.fn(),
+    }
+    const AuthenticationAccountModel = {
+      findOne: jest.fn(() => ({
+        exec: () => Promise.resolve(someUser),
+      })),
+    }
+    const response = await updatePassword({ AuthenticationAccountModel })({
+      _id,
+      password,
+      newPassword,
+    })
+    expect(AuthenticationAccountModel.findOne).toBeCalledWith({
+      $or: [{ _id }, { email: undefined }],
+    })
+    expect(someUser.verifyPassword).toBeCalledWith({
+      password,
+    })
+    expect(someUser.password).toBe(newPassword)
+    expect(someUser.save).toBeCalled()
+    expect(response).toEqual({
+      success: true,
+    })
+  })
 })
