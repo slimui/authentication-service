81ec61992221b0233a56e3eecbd416de9c5c61a3
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 17:07:26 -0500

    test: should complete a password reset by _id

--- tests/completePasswordReset.test.js
+++ tests/completePasswordReset.test.js
@@ -38,4 +38,37 @@ describe('completePasswordReset', () => {
       success: true,
     })
   })
+  it('should complete a password reset by _id', async () => {
+    const _id = 'some id'
+    const resetToken = 'some reset token'
+    const password = 'some password'
+    const someUser = {
+      verifyResetToken: jest.fn(() => true),
+      save: jest.fn(),
+    }
+    const AuthenticationAccountModel = {
+      findOne: jest.fn(() => ({
+        exec: () => Promise.resolve(someUser),
+      })),
+    }
+    const response = await completePasswordReset({
+      AuthenticationAccountModel,
+    })({
+      _id,
+      resetToken,
+      password,
+    })
+    expect(AuthenticationAccountModel.findOne).toBeCalledWith({
+      $or: [{ _id }, { email: undefined }],
+    })
+    expect(someUser.verifyResetToken).toBeCalledWith({
+      resetToken,
+    })
+    expect(someUser.password).toBe(password)
+    expect(someUser.resetToken).toBe(null)
+    expect(someUser.save).toBeCalled()
+    expect(response).toEqual({
+      success: true,
+    })
+  })
 })
