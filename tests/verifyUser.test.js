975a04afb4ddd35e65590d6f44a2aa6c0af15515
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 17:29:50 -0500

    test: should verify a user by email

--- tests/verifyUser.test.js
+++ tests/verifyUser.test.js
@@ -4,4 +4,44 @@ describe('verifyUser', () => {
   it('should export a function', () => {
     expect(verifyUser).toBeDefined()
   })
+  it('should verify a user by email', async () => {
+    const email = 'e@mail.com'
+    const password = 'some password'
+    const productName = 'some product name'
+    const foreignKey = 'foreignKey'
+    const someUser = {
+      verifyPassword: jest.fn(() => true),
+      verifyProductname: jest.fn(() => true),
+      productlinks: [
+        {
+          productName,
+          foreignKey,
+        },
+      ],
+    }
+    const AuthenticationAccountModel = {
+      findOne: jest.fn(() => ({
+        exec: () => Promise.resolve(someUser),
+      })),
+    }
+    const response = await verifyUser({
+      AuthenticationAccountModel,
+    })({
+      email,
+      password,
+      productName,
+    })
+    expect(AuthenticationAccountModel.findOne).toBeCalledWith({
+      $or: [{ _id: undefined }, { email }],
+    })
+    expect(someUser.verifyPassword).toBeCalledWith({
+      password,
+    })
+    expect(someUser.verifyProductname).toBeCalledWith({
+      productName,
+    })
+    expect(response).toEqual({
+      foreignKey,
+    })
+  })
 })
