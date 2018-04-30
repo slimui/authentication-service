848b6909936a39d455a0f2cde2ffe5e09fe8f343
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 16:22:43 -0500

    test: should start password reset by email

---
+++ __mocks__/uuid/v4.js
@@ -0,0 +1,4 @@
+const uniqueId = 'uniqueId'
+const v4 = jest.fn(() => uniqueId)
+v4.uniqueId = uniqueId
+module.exports = v4
--- tests/startPasswordReset.test.js
+++ tests/startPasswordReset.test.js
@@ -1,7 +1,41 @@
+const uuid = require('uuid/v4')
 const startPasswordReset = require('../src/startPasswordReset')
+let someDate = new Date()
 
 describe('startPasswordReset', () => {
+  beforeEach(() => {
+    global.Date = jest.fn(() => someDate)
+  })
+
+  afterEach(() => {
+    global.Date.mockRestore()
+  })
   it('should export a function', () => {
     expect(startPasswordReset).toBeDefined()
+  })
+
+  it('should start password reset by email', async () => {
+    const email = 'e@mail.com'
+    const AuthenticationAccountModel = {
+      updateOne: jest.fn(() =>
+        Promise.resolve({
+          n: 1,
+          ok: 1,
+        }),
+      ),
+    }
+    const response = await startPasswordReset({ AuthenticationAccountModel })({
+      email,
+    })
+    expect(AuthenticationAccountModel.updateOne).toBeCalledWith(
+      {
+        $or: [{ _id: undefined }, { email }],
+      },
+      { $set: { resetAt: someDate, resetToken: 'uniqueId' } },
+      { runValidators: true },
+    )
+    expect(response).toEqual({
+      resetToken: uuid.uniqueId,
+    })
   })
 })
