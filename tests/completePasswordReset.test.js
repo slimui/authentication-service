a250c0a5f68b6a30d903d0850944cdb5629002a9
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 17:06:16 -0500

    test: should complete a password reset by email

--- tests/completePasswordReset.test.js
+++ tests/completePasswordReset.test.js
@@ -4,4 +4,38 @@ describe('completePasswordReset', () => {
   it('should export a function', () => {
     expect(completePasswordReset).toBeDefined()
   })
+
+  it('should complete a password reset by email', async () => {
+    const email = 'e@mail.com'
+    const resetToken = 'some reset token'
+    const password = 'some password'
+    const someUser = {
+      verifyResetToken: jest.fn(() => true),
+      save: jest.fn(),
+    }
+    const AuthenticationAccountModel = {
+      findOne: jest.fn(() => ({
+        exec: () => Promise.resolve(someUser),
+      })),
+    }
+    const response = await completePasswordReset({
+      AuthenticationAccountModel,
+    })({
+      email,
+      resetToken,
+      password,
+    })
+    expect(AuthenticationAccountModel.findOne).toBeCalledWith({
+      $or: [{ _id: undefined }, { email }],
+    })
+    expect(someUser.verifyResetToken).toBeCalledWith({
+      resetToken,
+    })
+    expect(someUser.password).toBe(password)
+    expect(someUser.resetToken).toBe(null)
+    expect(someUser.save).toBeCalled()
+    expect(response).toEqual({
+      success: true,
+    })
+  })
 })
