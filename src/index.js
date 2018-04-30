41c395c5febffb8cd880b3eb01ca5186d26c3003
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 15:37:32 -0500

    cleanup commented out code from prototype

--- src/index.js
+++ src/index.js
@@ -6,26 +6,14 @@ const getUser = require('./getUser')
 const createProductlink = require('./createProductlink')
 const removeProductlink = require('./removeProductlink')
 const updatePassword = require('./updatePassword')
-// const passwordResetStart = require('./passwordResetStart')
+const startPasswordReset = require('./startPasswordReset')
 // const passwordResetComplete = require('./passwordResetComplete')
 // const verify = require('./verify')
 const { rpc, method } = require('@bufferapp/micro-rpc')
 const app = express()
-// app.use(bodyParser.json())
-
-// const promisifiedMongoClient = promisify(MongoClient)
 
 const initDB = async () => {
   await mongoose.connect(process.env.MONGO_URL)
-  // const client = await promisifiedMongoClient.connect(process.env.MONGO_URL)
-  // // TODO: check if index exists first
-  // const collectionClient = client
-  //   .db(process.env.MONGO_DB)
-  //   .collection('authenticationAccounts')
-  // await collectionClient.createIndex({ email: 1 }, { unique: 1 })
-  // return {
-  //   collectionClient,
-  // }
   return {
     AuthenticationAccountModel: AuthenticationAccountModel({
       mongooseConnection: mongoose,
@@ -35,29 +23,6 @@ const initDB = async () => {
 
 const main = async () => {
   const { AuthenticationAccountModel } = await initDB()
-  // const authenticationAccount = new AuthenticationAccountModel({
-  //   email: 'e@mail.com',
-  //   password: 'some password',
-  // })
-  // await authenticationAccount.save()
-  // const data = await AuthenticationAccountModel.find().exec()
-  // console.log('data', data)
-  // const { collectionClient } = await initDB()
-  // app.post('/api/create', create({ collectionClient }))
-  // app.post('/api/get', get({ collectionClient }))
-  // app.post('/api/productlinks/create', productlinksCreate({ collectionClient }))
-  // app.post('/api/productlinks/remove', productlinksRemove({ collectionClient }))
-  // app.post('/api/password/update', passwordUpdate({ collectionClient }))
-  // app.post(
-  //   '/api/password/reset/start',
-  //   passwordResetStart({ collectionClient }),
-  // )
-  // app.post(
-  //   '/api/password/reset/complete',
-  //   passwordResetComplete({ collectionClient }),
-  // )
-  // app.post('/api/verify', verify({ collectionClient }))
-
   app.post('/rpc', (req, res, next) => {
     rpc(
       method('createUser', createUser({ AuthenticationAccountModel })),
@@ -71,6 +36,10 @@ const main = async () => {
         removeProductlink({ AuthenticationAccountModel }),
       ),
       method('updatePassword', updatePassword({ AuthenticationAccountModel })),
+      method(
+        'startPasswordReset',
+        startPasswordReset({ AuthenticationAccountModel }),
+      ),
     )(req, res).catch(err => next(err))
   })
   app.listen(80, () => console.log('Started listening on port 80'))
