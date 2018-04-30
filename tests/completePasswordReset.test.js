eb164144a34d7854f04e70b177b7e1952b4bbdf6
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 17:01:52 -0500

    test: should export a function

---
+++ tests/completePasswordReset.test.js
@@ -0,0 +1,7 @@
+const completePasswordReset = require('../src/completePasswordReset')
+
+describe('completePasswordReset', () => {
+  it('should export a function', () => {
+    expect(completePasswordReset).toBeDefined()
+  })
+})
