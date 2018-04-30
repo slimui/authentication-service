fc6aaf41d766ca017721566746dca4712b2e0de1
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 15:55:51 -0500

    implement start password reset

--- src/startPasswordReset.js
+++ src/startPasswordReset.js
@@ -1,55 +1,34 @@
-const { ObjectID } = require('mongodb')
 const uuid = require('uuid/v4')
-const Joi = require('joi')
-const {
-  validate,
-  parseValidationErrorMessage,
-} = require('./utils')
+const { createError } = require('@bufferapp/micro-rpc')
 
-const schema = Joi.object()
-  .keys({
-    email: Joi.string(),
-    id: Joi.string(),
-  })
-    .xor('email', 'id')
-
-module.exports = ({ collectionClient }) => async (req, res) => {
-  try {
-    await validate({
-      value: req.body,
-      schema
+module.exports = ({ AuthenticationAccountModel }) => async ({ _id, email }) => {
+  if (!_id && !email) {
+    throw createError({
+      message: 'Please specify an _id or email',
     })
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      message: parseValidationErrorMessage({ error }),
-    })
-  }
-  const { email, id } = req.body
-  let query
-  if (email) {
-    query = { email }
-  } else {
-    query = {
-      _id: ObjectID(id),
-    }
   }
   const resetToken = uuid()
-  const result = await collectionClient.updateOne(query, {
-    $set: {
-      resetAt: new Date(),
-      resetToken,
+  const result = await AuthenticationAccountModel.updateOne(
+    { $or: [{ _id }, { email }] },
+    {
+      $set: {
+        resetAt: new Date(),
+        resetToken,
+      },
     },
-  })
-  if (result.matchedCount !== 1) {
-    res.status(400).send({
-      success: false,
-      message: `Could not start password reset on account with ${ email ? 'email' : 'id' }: ${ email ? email : id }`
+    { runValidators: true },
+  )
+  if (result.n !== 1 || result.ok !== 1) {
+    throw createError({
+      message: `Could not start password reset on account with ${JSON.stringify(
+        {
+          $or: [{ _id }, { email }],
+        },
+      )}`,
     })
   } else {
-    res.send({
-      success: true,
+    return {
       resetToken,
-    })
+    }
   }
 }
