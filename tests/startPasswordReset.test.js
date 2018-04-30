87ca2d9913080579410604ee93195bd859ff1bb1
Harrison Harnisch <hharnisc@gmail.com>
2018-04-30 16:04:03 -0500

    add existence test for startPasswordReset

---
+++ tests/startPasswordReset.test.js
@@ -0,0 +1,7 @@
+const startPasswordReset = require('../src/startPasswordReset')
+
+describe('startPasswordReset', () => {
+  it('should export a function', () => {
+    expect(startPasswordReset).toBeDefined()
+  })
+})
